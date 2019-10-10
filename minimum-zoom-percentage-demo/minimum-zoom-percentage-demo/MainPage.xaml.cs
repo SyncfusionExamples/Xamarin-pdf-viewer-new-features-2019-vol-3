51558fc05ee6db2043156eb9fe858000cddd7f3b
Ramkumar Ravy <ramkumarr@syncfusion.com>
2019-10-10 10:19:12 +0530

    Added Single page view and Minimum zoom percentage samples

---
+++ minimum-zoom-percentage-demo/minimum-zoom-percentage-demo/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace minimum_zoom_percentage_demo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			//Read the PDF into a Stream
+			Stream stream = typeof(MainPage).GetTypeInfo().Assembly.GetManifestResourceStream("minimum-zoom-percentage-demo.Assets.GIS Succinctly.pdf");
+
+			//Load the Stream into SfPdfViewer
+			pdfViewer.LoadDocument(stream);
+
+			//Set the minimum zoom percentage value to ‘10’ (programatically)
+			//pdfViewer.MinimumZoomPercentage = 50;
+		}
+
+	}
+}
---
+++ single-page-view-demo/single-page-view-demo/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace single_page_view_demo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			//Read the PDF into a Stream
+			Stream stream = typeof(MainPage).GetTypeInfo().Assembly.GetManifestResourceStream("single-page-view-demo.Assets.GIS Succinctly.pdf");
+
+			//Load the Stream into SfPdfViewer
+			pdfViewer.LoadDocument(stream);
+
+			//Enable the Single page view mode (programatically)
+			//pdfViewer.PageViewMode = Syncfusion.SfPdfViewer.XForms.PageViewMode.PageByPage;
+		}
+	}
+}
